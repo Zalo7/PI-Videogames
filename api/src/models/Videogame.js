467e35c8d80e3cead2bcf1f75d16225e8a64310c
Zalo7 <test@youremail.com>
2021-09-27 21:39:24 -0300

    DB Done

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,38 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    releaseDate: {
+      type: DataTypes.STRING,
+    },
+    rating: {
+      type: DataTypes.STRING,
+    },
+    plataforms: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    }
   });
 };
